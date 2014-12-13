caf35b17517e466ad0901ffe961981c76b9e3e62
wcong <wc19920415@gmail.com>
2014-12-13 16:46:37 +0800

    add web frame

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # encoding=utf8
 
+import sys
+
 import web
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 '''
 ok this is our main page
@@ -10,14 +15,21 @@ ok this is our main page
 urls = (
     '/', 'Index'
 )
+web.config.debug = True
+render = web.template.render('templates/', globals={'str': str}, base="index_layout", cache=False)
+
+
+def login_hook(handle):
+    web.cookies().get("login")
 
 
 class Index:
     def GET(self):
-        return "hello world"
+        return render.index()
 
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
+    app.add_processor(login_hook)
     app.run()
 
