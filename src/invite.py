7cb79d1d8ce056ee30f670c2317b5f0098ade3f0
cong.wang <cong.wang@tqmall.com>
2014-12-16 15:34:10 +0800

    miss path /

--- src/invite.py
+++ src/invite.py
@@ -27,7 +27,7 @@ class Index:
         email = input.get("email")
 
         register_link_meta = util.make_register_link(email)
-        register_link = 'http://' + config.host + '/register?email=' + register_link_meta[0] + '&sign=' + \
+        register_link = 'http://' + config.host + '/register/?email=' + register_link_meta[0] + '&sign=' + \
                         register_link_meta[1]
         Index.select_or_add_email(email, register_link_meta[1])
         message = '<html xmlns=\'http://www.w3.org/1999/xhtml\'><body><h1>click the follow links</h1><a href="' + register_link + '">click to register</a></body></html>'
@@ -42,7 +42,7 @@ class Index:
         user_id = pdbc.User.select_id_by_email(email)
         if user_id is not None:
             pdbc.User.update_by_id(str(user_id), 'check_sign', check_sign)
-            return 
+            return
         company_email = util.extract_company_from_email(email)
         company_id = pdbc.Company.select_id_by_email(company_email)
         if company_id is None:
@@ -50,7 +50,7 @@ class Index:
             company_id = pdbc.Company.select_id_by_email(company_email)
             pdbc.Position.init_company(str(company_id))
         position_id = pdbc.Position.select_id_by_company(str(company_id))
-        pdbc.User.init_user(email, check_sign, str(company_id),str(position_id))
+        pdbc.User.init_user(email, check_sign, str(company_id), str(position_id))
 
 
 app_invite = web.application(urls, locals())
