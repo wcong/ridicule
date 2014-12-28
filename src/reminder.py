c6230f6845a519d1c20be9d191c003f35b10e564
cong.wang <cong.wang@tqmall.com>
2014-12-28 21:54:56 +0800

    add missing file

---
+++ src/reminder.py
@@ -0,0 +1,39 @@
+# encoding=utf8
+'''
+notice page
+'''
+
+import web
+import json
+import config
+import pdbc
+import util
+
+urls = (
+    '/', 'Index'
+)
+
+
+class Index():
+    def GET(self):
+        if web.ctx.get('Accept').lower() == 'json':
+            return self.get_json()
+        else:
+            return self.get_html()
+
+    def get_json(self):
+        email = util.get_user_email()
+        user_id = pdbc.User.select_id_by_email(email)
+        like_list = pdbc.ReminderLike.select_reminder(user_id)
+        friend_list = pdbc.ReminderFriend.select_reminder(user_id)
+        comment_list = pdbc.ReminderComment.select_reminder(user_id)
+        data = dict()
+        data['like_list'] = like_list
+        data['friend_list'] = friend_list
+        data['comment_list'] = comment_list
+        return json.dumps(data)
+
+    def get_html(self):
+        return config.render.notice({"hello": "world"})
+
+app_reminder = web.application(urls,locals())
---
+++ static/js/app/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+});
+function get_notice(){
+    $.getJSON('/notice',function(data){
+	    
+    })
+}
